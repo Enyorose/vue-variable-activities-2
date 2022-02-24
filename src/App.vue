14bc93d640da114d2a963295e546690f9b03190c
Jessica.W <jessica.watson1@live.com>
2022-02-24 12:12:44 -0700

    set up commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,54 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="flex justify-center">
+    <article class="max-w-sm rounded overflow-hidden shadow-lg mt-4 p-4">
+      <!-- replace all text in <strong> with double moustache dynamic variable -->
+      <h1 class="text-xl mb-2">
+        Hello my name is <strong>{{ users[0].name }}</strong>
+      </h1>
+      <p class="text-gray-700 text-base">
+        I lived in <strong>Brooks</strong> Alberta
+      </p>
+      <p class="text-gray-700 text-base">
+        My favorite genre of books is <strong>fantasy!</strong>
+      </p>
+    </article>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Vue from "vue";
+import VueCompositionAPI from "@vue/composition-api";
+
+Vue.use(VueCompositionAPI);
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
+  setup() {
+    const users = [
+      {
+        name: "Alex",
+        place: "Calgary",
+      },
+      {
+        name: "Rolf",
+        place: "Calgary",
+      },
+      {
+        name: "Maria",
+        place: "Calgary",
+      },
+      {
+        bookGenres: ["fantasy", "mystery", "paranormal", "comedy"],
+      },
+    ];
+
+    return {
+      users,
+    };
   },
 };
 </script>
 
-<style lang="scss">
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -26,3 +58,4 @@ export default {
   margin-top: 60px;
 }
 </style>
+
